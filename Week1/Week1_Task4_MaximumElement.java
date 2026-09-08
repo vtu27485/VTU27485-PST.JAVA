2fbab30b408b8040923c0c107038b73592fce3cd
vtu27485 <vtu27485@veltech.edu.in>
2026-09-08 09:49:44 +0530

    Update Week1_Task4_MaximumElement.java

--- Week1/Week1_Task4_MaximumElement.java
+++ Week1/Week1_Task4_MaximumElement.java
@@ -1,4 +1,5 @@
 package Week_Tasks.Week1_Tasks;
+
 import java.util.Scanner;
 
 public class Week1_Task4_MaximumElement {
@@ -9,12 +10,14 @@ public class Week1_Task4_MaximumElement {
         int n = sc.nextInt();
 
         int[] arr = new int[n];
+
         System.out.println("Enter " + n + " elements:");
         for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
         }
 
         int max = arr[0];
+
         for (int i = 1; i < n; i++) {
             if (arr[i] > max) {
                 max = arr[i];
@@ -22,5 +25,18 @@ public class Week1_Task4_MaximumElement {
         }
 
         System.out.println("Maximum element is: " + max);
+
+        sc.close();
     }
 }
+
+/*
+Sample Input:
+5
+25 10 45 30 20
+
+Sample Output:
+Enter number of elements: 5
+Enter 5 elements:
+Maximum element is: 45
+*/
