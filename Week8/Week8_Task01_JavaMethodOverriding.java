67e1e5dd9369afe78dbc88668556d482b97ca6eb
vtu27485 <vtu27485@veltech.edu.in>
2026-09-24 09:23:28 +0530

    Create Week8_Task01_JavaMethodOverriding.java

---
+++ Week8/Week8_Task01_JavaMethodOverriding.java
@@ -0,0 +1,43 @@
+package Week_Tasks.Week8_Tasks;
+
+public class Week8_Task1_JavaMethodOverriding {
+    static class Sports {
+        String getName() {
+            return "Generic Sports";
+        }
+
+        void getNumberOfTeamMembers(int n) {
+            System.out.println("Each team has " + n + " players in " + getName());
+        }
+    }
+
+    static class Soccer extends Sports {
+        @Override
+        String getName() {
+            return "Soccer Class";
+        }
+
+        @Override
+        void getNumberOfTeamMembers(int n) {
+            System.out.println("Each team has 11 players in " + getName());
+        }
+    }
+
+    public static void main(String[] args) {
+        Sports sports = new Sports();
+        System.out.println(sports.getName());
+        sports.getNumberOfTeamMembers(11);
+
+        Sports soccer = new Soccer();
+        System.out.println(soccer.getName());
+        soccer.getNumberOfTeamMembers(11);
+    }
+}
+
+/*
+OUTPUT:
+Generic Sports
+Each team has 11 players in Generic Sports
+Soccer Class
+Each team has 11 players in Soccer Class
+*/
